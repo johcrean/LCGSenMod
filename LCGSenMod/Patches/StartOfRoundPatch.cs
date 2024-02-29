5dc7165120d741fe7228af45b99c3f548983613a
John <johcrean1995@gmail.com>
2024-02-29 00:16:11 -0500

    Add project files.

---
+++ LCGSenMod/Patches/StartOfRoundPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCGSenMod.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartOfRoundPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void OverrideAudio(StartOfRound __instance)
+        {
+            __instance.alarmSFX = GSenModBase.SoundFX[0];
+        }
+    }
+}
---
+++ LCGSenMod/Plugin.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using LCGSenMod.Patches;
+
+namespace LCGSenMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class GSenModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "ChaosCrab.LCGSMod";
+        private const string modName = "LC GS Mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static GSenModBase Instance;
+
+        internal ManualLogSource mls;
+
+        internal static List<AudioClip> SoundFX;
+        internal static AssetBundle Bundle;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Mod Awake");
+
+            harmony.PatchAll(typeof(StartOfRoundPatch));
+
+            SoundFX = new List<AudioClip>();
+            string FolderLocation = Instance.Info.Location;
+            FolderLocation = FolderLocation.TrimEnd("LCGSenMod.dll".ToCharArray());
+            Bundle = AssetBundle.LoadFromFile(FolderLocation + "sengancut");
+
+            if(Bundle != null)
+            {
+                mls.LogInfo("Loaded Asset Bundle");
+                SoundFX = Bundle.LoadAllAssets<AudioClip>().ToList();
+            }
+            else
+            {
+                mls.LogError("Failed to Load Bundle");
+            }
+        }
+    }
+}
